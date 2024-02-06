e354180313509d089e6f2e415e46fe06065ae61f
Ubuntu <ubuntu@ip-172-31-33-107.ec2.internal>
2024-02-06 07:27:53 +0000

    task4

---
+++ task4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#name=$1
+#age=$2
+#eduction=$3
+
+echo "my name is :$1 my age:$2 my eduction is $3 my file name is $0"
+
+echo $8 ${10}  $11
+echo "total parameters =$# "
+
+echo "what are the all parameters =$@"
+echo "parameters* used in script is : =$*"
+
+
